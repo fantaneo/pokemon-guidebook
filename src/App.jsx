0868527ad8ad46b4f34010c4e87562ce345c69c6
ryuji-i <fantaneo.orange@gmail.com>
2024-06-22 22:25:04 +0900

    Refactor image URL generation into a helper function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ function App() {
       .then((data) => setPokemons(data.results));
   }, []);
 
+  // Helper function to extract Pokemon ID from URL and form the image URL
+  const getPokemonImageUrl = (url) => {
+    const id = url.split("/").filter(Boolean).pop(); // Extracts the last non-empty segment of the URL
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+  };
+
   return (
     <div className="p-5">
       <div className="grid grid-cols-3 gap-4">
@@ -21,9 +27,7 @@ function App() {
           >
             <h1 className="text-lg font-bold">{pokemon.name}</h1>
             <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-                pokemon.url.split("/")[pokemon.url.split("/").length - 2]
-              }.png`}
+              src={getPokemonImageUrl(pokemon.url)}
               alt={pokemon.name}
               className="w-20 h-20"
               loading="lazy"
