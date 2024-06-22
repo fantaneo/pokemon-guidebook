4d246543e6b4e229507fdc5d6503da11d25ea683
ryuji-i <fantaneo.orange@gmail.com>
2024-06-22 20:48:45 +0900

    Display Pokemon names in the UI from PokeAPI data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
+import { useState, useEffect } from "react";
+
 function App() {
-  return <></>;
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon")
+      .then((res) => res.json())
+      .then((data) => setPokemons(data.results));
+  }, []);
+
+  return (
+    <div>
+      {pokemons.map((pokemon) => (
+        <div key={pokemon.name}>
+          <h1>{pokemon.name}</h1>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
